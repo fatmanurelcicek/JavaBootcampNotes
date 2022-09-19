71b3f3da69ee7c9b3377b04a2a7f5e232a8b908f
fatmanurelcicek <fatmanurelcicek002@gmail.com>
2022-09-19 23:05:55 +0300

    ogrenci sayisi yazdim

---
+++ ödev2/degiskenler.java
@@ -0,0 +1,15 @@
+package ödev2;
+public class degiskenler {
+ public static void main(String[] args) {
+            int ogrenciSayisi=12;
+            String mesaj="ogrenci sayisi: ";
+            System.out.println(mesaj+ogrenciSayisi);
+            System.out.println(mesaj+ogrenciSayisi);
+            System.out.println("ogrenci sayim: "+ogrenciSayisi);
+            System.out.println("ogrenci sayim: "+ogrenciSayisi);
+            System.out.println("ogrenci sayim: "+ogrenciSayisi);
+    
+        }
+    
+    }
+
