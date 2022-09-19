d51c8c65958b4b9c5b4a73943c5450ce2b10f074
fatmanurelcicek <fatmanurelcicek002@gmail.com>
2022-09-20 01:28:32 +0300

    kalinincesesliharf yazdım

---
+++ ödev2/kalinincesesliharfler.java
@@ -0,0 +1,19 @@
+package ödev2;
+
+public class kalinincesesliharfler {
+    
+	public static void main(String[] args) {
+		char harf='E';
+		switch(harf) {
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U':
+			System.out.println("Kalin sesli harf");
+			break;
+			default:
+				System.out.println("ince sesli harf");
+		}
+
+	}
+}
