8154eb497e30c5de6f31a5d7cfa35194763fd5e0
fatmanurelcicek <fatmanurelcicek002@gmail.com>
2022-09-20 01:32:22 +0300

    sayı bulma yaptım

---
+++ ödev2/sayibulma.java
@@ -0,0 +1,23 @@
+package ödev2;
+
+public class sayibulma {
+   
+	public static void main(String[] args) {
+		int[]sayilar=new int[] {1,2,5,7,9,0};
+		int aranacak=5;
+		boolean varMi=false;
+		
+		for(int sayi:sayilar) {
+			if(sayi==aranacak) {
+				varMi=true;
+				break;
+			}
+		}
+		if(varMi) {
+			System.out.println("Sayi mevcuttur.");
+		}else {
+			System.out.println("Sayi mevcut değildir.");
+		}
+		
+	} 
+}
