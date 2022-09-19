ae2f513eec7a056353969fde1958c36abe8879e7
fatmanurelcicek <fatmanurelcicek002@gmail.com>
2022-09-20 01:26:27 +0300

    if blokları yazdım

---
+++ ödev2/ifblokları.java
@@ -0,0 +1,18 @@
+package ödev2;
+
+public class ifblokları {
+   
+	public static void main(String[] args) {
+        int sayi=65;
+        if(sayi<40) {
+            System.out.println("Sayi 40'den küçüktür.");
+        }else if (sayi==30) {
+            System.out.println("Sayi 40'ye eşittir.");
+            
+        }else {
+            System.out.println("Sayi 40'den büyüktür.");
+        }
+    
+        }
+     
+}
