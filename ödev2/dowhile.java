821401aa5850b5a6cd1644bf8b482f83788ebc52
fatmanurelcicek <fatmanurelcicek002@gmail.com>
2022-09-20 01:20:06 +0300

    do while yazdım

---
+++ ödev2/dowhile.java
@@ -0,0 +1,16 @@
+package ödev2;
+
+public class dowhile {
+  
+	public static void main(String[] args) {
+		int k=100;
+		do {
+			System.out.println(j);
+			k+=2;
+		}while(k<10);
+		System.out.println("Do-While döngüsü bitti.");
+	}
+	
+
+}  
+}
