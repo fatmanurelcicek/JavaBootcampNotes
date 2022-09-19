2ecb7d91a9b4bf5e65fd5684480e04fa13d1424a
fatmanurelcicek <fatmanurelcicek002@gmail.com>
2022-09-20 01:23:53 +0300

    for dongusu yazdım

---
+++ ödev2/fordongusu.java
@@ -0,0 +1,15 @@
+package ödev2;
+
+public class fordongusu {
+    public static void main(String[] args) {
+		for (int i=4;i<20;i+=4) {
+			System.out.println(i);
+			
+			
+		}
+		System.out.println("Döngü bitti");
+
+	}
+
+}
+
