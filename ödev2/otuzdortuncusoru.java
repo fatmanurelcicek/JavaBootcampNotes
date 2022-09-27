49084d3fb207c23aaf9be5811b53ee43c0a4b421
fatmanurelcicek <fatmanurelcicek002@gmail.com>
2022-09-27 10:43:07 +0300

    CLLASLAR YAZDIM

---
+++ ödev2/otuzdortuncusoru.java
@@ -0,0 +1,21 @@
+public class otuzdortuncusoru {
+    
+
+public static void main(String[] args) {
+    int[] sayilar =new int[] (1,2,5,7,9,0);
+    int aranacak=6;
+    boolean varMi=false;
+
+    for (int sayi : sayilar){
+        if(sayi==aranacak){
+            varMi = true;
+            break;
+        }
+    }
+    if(varMi){
+      System.out.println("sayı mevcuttur");
+    }else{
+        System.out.println("sayı mevcut değildir");
+    }  
+    }
+}
