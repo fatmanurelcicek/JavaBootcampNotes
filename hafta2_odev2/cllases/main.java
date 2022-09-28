9194740ea74c87bc8c75f7fe683c8945f0c6e7d4
fatmanurelcicek <fatmanurelcicek002@gmail.com>
2022-09-28 08:10:01 +0300

    referansları anladım

---
+++ hafta2_odev2/cllases/main.java
@@ -0,0 +1,31 @@
+package hafta2_odev2.cllases;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------");
+
+        //reference type
+        CustomerManager customerManager = new CustomerManager();
+        
+        customerManager.Add();
+        customerManager.Remove();
+        customerManager.Update();
+
+        //Value
+        int sayi1 = 10;
+        int sayi2 = 20;
+        sayi2 = sayi1;
+        sayi1=30;
+        System.out.println(sayi2);
+
+        int[] numbers1 = new int[]{1,2,3};
+        int[] numbers2 = new int[]{4,5,6};
+
+        numbers2 = numbers1;
+        numbers1[0]=10;
+        System.out.println(numbers2[0]);
+
+        System.out.println("--------------------------------");
+    }
+}
+
