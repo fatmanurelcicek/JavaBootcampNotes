1482ed180f4ca4c4f5866600521b8e5ab6317042
fatmanurelcicek <fatmanurelcicek002@gmail.com>
2022-09-28 09:04:59 +0300

    Basit Bir Hesap Makinesi yapıldı

---
+++ hafta2_odev2/ReCapDemo_Classes/DortIslem.java
@@ -0,0 +1,19 @@
+package hafta2_odev2.ReCapDemo_Classes;
+public class DortIslem {
+
+    public int Topla(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public int Cikar(int num1, int num2){
+        return num1 - num2;
+    }
+
+    public int Carp(int num1, int num2){
+        return num1 * num2;
+    }
+
+    public int Bol(int num1, int num2){
+        return num1 / num2;
+    }
+}
