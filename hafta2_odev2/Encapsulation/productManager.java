bbacc9ef0d4008c71838ad688ec06673dbbc945b
fatmanurelcicek <fatmanurelcicek002@gmail.com>
2022-09-28 08:54:19 +0300

    Getter & Setter Methodlarını Kullandım

---
+++ hafta2_odev2/Encapsulation/productManager.java
@@ -0,0 +1,14 @@
+package hafta2_odev2.Encapsulation;
+
+
+class ProductManager{
+    public void Add(Product product){
+        
+        //JDBC
+        System.out.println("Added Product ---> " + product.name);
+    }
+
+    public void Add2(int id, String name, String description, int stock, double price ){
+
+    }
+}
