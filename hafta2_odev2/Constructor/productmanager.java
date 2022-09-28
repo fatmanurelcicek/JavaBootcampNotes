96387be2cf82153b4eddef238cd67a89940f0533
fatmanurelcicek <fatmanurelcicek002@gmail.com>
2022-09-28 08:47:21 +0300

    manager eklendi

---
+++ hafta2_odev2/Constructor/productmanager.java
@@ -0,0 +1,14 @@
+package hafta2_odev2.Constructor;
+
+public class productmanager {
+    public void Add(Product product){
+        
+        //JDBC
+        //System.out.println("Added Product ---> " + product.setName);
+    }
+
+    public void Add2(int id, String name, String description, int stock, double price ){
+
+    }
+} 
+
