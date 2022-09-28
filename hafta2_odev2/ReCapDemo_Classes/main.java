1e0d98641fd1b14ea11a070d955ae2d79fc45f2f
fatmanurelcicek <fatmanurelcicek002@gmail.com>
2022-09-28 09:06:04 +0300

    basit bir hesap makinası yapıldı

---
+++ hafta2_odev2/ReCapDemo_Classes/main.java
@@ -0,0 +1,10 @@
+package hafta2_odev2.ReCapDemo_Classes;
+
+public class Main {
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
+        
+        int sonuc = dortIslem.Carp(2, 5);
+        System.out.println(sonuc); 
+    }
+}
