b329077d933335cb70927b84b3b1cbc936220c70
fatmanurelcicek <fatmanurelcicek002@gmail.com>
2022-09-28 08:42:35 +0300

    main methodu oluşturuldu

---
+++ hafta2_odev2/Constructor/main.java
@@ -0,0 +1,20 @@
+package hafta2_odev2.Constructor;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------");
+    
+    
+    
+        Product product = new Product(1,"MacBook","Laptop",5,"Uzay Grisi");
+        
+        ProductManager productManager = new ProductManager();
+
+        productManager.Add(product);
+
+        System.out.println("--------------------------------------");
+    }
+} 
+    
+
