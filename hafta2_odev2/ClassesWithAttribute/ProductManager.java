2d949cf32bf6e99e5ec76f78f9fb5dbead123987
fatmanurelcicek <fatmanurelcicek002@gmail.com>
2022-09-28 08:21:27 +0300

    rename

---
+++ hafta2_odev2/ClassesWithAttribute/ProductManager.java
@@ -0,0 +1,11 @@
+package hafta2_odev2.ClassesWithAttribute;
+
+public public class ProductManager {
+    public void Add(Product product){
+        
+        //JDBC
+        System.out.println("Added Product ---> " + product.name);
+    }
+} 
+    
+
