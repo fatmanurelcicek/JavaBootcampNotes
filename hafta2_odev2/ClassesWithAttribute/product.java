527396702e41d0359209375a65e31a163aa1c263
fatmanurelcicek <fatmanurelcicek002@gmail.com>
2022-09-28 08:18:32 +0300

    product nesnemi oluşturdum

---
+++ hafta2_odev2/ClassesWithAttribute/product.java
@@ -0,0 +1,10 @@
+package hafta2_odev2.ClassesWithAttribute;
+
+public class Product {
+    //Attribute | Field
+    int id;
+    String name;
+    String description;
+    double price;
+    int stock;
+}
