73118595adc5af02c83620091cb2916167a0b68e
fatmanurelcicek <fatmanurelcicek002@gmail.com>
2022-09-28 08:44:26 +0300

    yapıcı method oluşturuldu

---
+++ hafta2_odev2/Constructor/product.java
@@ -0,0 +1,75 @@
+package hafta2_odev2.Constructor;
+
+public public class Product {
+
+    public Product(int id, String name, String description, int stock, String renk){
+
+        System.out.println(" 'Constructor Method Running' ");
+    }
+
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private int stock;
+    private String color;
+    private String productCode;
+
+    public String getProductCode() {
+        return this.productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName(String name) {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return this.stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+} 
+    
+
